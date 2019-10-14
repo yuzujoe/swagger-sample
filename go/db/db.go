ee54a7970ea2f05a8a92dad4964d7aa575d435c3
ohira yuzuru <yuzu-joe@ichain.co.jp>
2019-10-15 00:20:16 +0900

    変数名DbをDBに変更

--- go/db/db.go
+++ go/db/db.go
@@ -10,8 +10,8 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-// Db for gormDB
-var Db *gorm.DB
+// DB for gormDB
+var DB *gorm.DB
 
 // Init gormでmysqlへの接続データベースの作成
 func Init() *gorm.DB {
@@ -19,19 +19,19 @@ func Init() *gorm.DB {
 
 	path := strings.Join([]string{os.Getenv("MYSQL_USER"), ":", os.Getenv("MYSQL_PASSWORD"), "@tcp(", os.Getenv("DB_HOST"), ":", os.Getenv("DB_PORT"), ")/", os.Getenv("MYSQL_DATABASE"), "?charset=utf8&parseTime=True&loc=Local"}, "")
 
-	Db, err = gorm.Open("mysql", path)
+	DB, err = gorm.Open("mysql", path)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Successfully connected!")
-	Db.LogMode(true)
+	DB.LogMode(true)
 
-	err = Db.DB().Ping()
+	err = DB.DB().Ping()
 	if err != nil {
 		panic(err)
 	}
 
-	Db.AutoMigrate(&models.Reservation{}, &models.User{})
+	DB.AutoMigrate(&models.Reservation{}, &models.User{})
 
-	return Db
+	return DB
 }
